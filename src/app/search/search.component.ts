910c7c30832a4f0b41e5c41be044b29df8c1f93c
David Montero <a15davmonceb@iam.cat>
2017-05-15 10:46:25 +0200

    Nuevo buscador

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'buscador',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class BuscadorComponent{
+
+}

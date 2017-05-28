1e27c27f01208fa6958b9e0c5d00df5d1ed9f3bd
jorgeartigas <jorgeed_94@hotmail.com>
2017-05-28 23:03:37 +0200

    funcionalidades admin panel working

--- src/app/panel/edit-news.component.ts
+++ src/app/panel/edit-news.component.ts
@@ -11,7 +11,7 @@ import { Noticia } from '../festivales/noticia';
 export class EditNewsComponent implements OnInit {
   idNoticia:string;
   noticias:any;
-  noticia: Noticia = new Noticia("","","","",Date.now());
+  noticiaEdit: Noticia = new Noticia("","","","",Date.now());
   publicada: boolean = false;
   editando: boolean = false;
     constructor(
@@ -20,16 +20,14 @@ export class EditNewsComponent implements OnInit {
                 public route: ActivatedRoute,
     ){}
     ngOnInit(){
-        this.af.database.object('FESTIVALERS/noticias/generales/').first().subscribe(noticias =>{
+        this.af.database.list('FESTIVALERS/noticias/generales/').first().subscribe(noticias =>{
           if(noticias[0]){
             this.noticias = noticias;
             console.log(this.noticias)
-          }else{
-            console.log('no hay')
           }
         })
     }
-    update(){
-      this.af.database.object('FESTIVALERS/noticias/generales/'+this.idNoticia).update(this.noticia);
+    updateNew(){
+      this.af.database.object('FESTIVALERS/noticias/generales/'+this.idNoticia).update(this.noticiaEdit);
     }
 }
